a7ec502a10f0aa750a02040f552b6d2edae21789
Ricky <rickyskywalker@berkeley.edu>
2021-09-27 15:22:46 +0800

    27 Sep 2021

---
+++ Candle.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class OneDayTrade {
+    public double open;
+    public double close;
+    public double high;
+    public double low;
+    public double volume;
+
+    public OneDayTrade(LinkedList<Trade>){
+
+    }
+
+}
